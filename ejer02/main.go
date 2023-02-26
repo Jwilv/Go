b8c6714c7153670f4f957226eb56419050aa72d2
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-26 13:43:52 -0300

    creacion y asignacion de variables

---
+++ ejer02/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+//declaracion de variables
+var numero int
+var cadenaDeTexto string
+var boolean bool
+
+//manera pro
+// numero2 := 1
+
+func main() {
+	numero3, numero4, numero5, texto1 := 1, 2, 3, "cadena de texto"
+
+	fmt.Println(numero3)
+	fmt.Println(numero4)
+	fmt.Println(numero5)
+	fmt.Println(texto1)
+}
