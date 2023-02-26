e8812597b108260b6d1547a245edb765f9518b94
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-02-26 14:54:29 -0300

    explicacion de condiciones

--- ejer03/main.go
+++ ejer03/main.go
@@ -8,10 +8,23 @@ func main(){
 
 	state = true 
 
+	//los parentecis () no son necesario al menos que
+	//se necesite por ej
+	//hacer un calculo matematico 
+	// (1+2)*(2+3)
+
 	if state == true {
 		fmt.Println(state);
 	}else{
 		fmt.Println("es falso")
+	}
+
+	//se sigue la misma regla, el inicio del corchete tiene que estar en la misma linea
+
+	//se le puede asignar un valor a una variable en la misma linea del if
+	// y luego condicionarlo 
+	if state=false; state == false{
+		fmt.Println("se asigno")
 	}
 
 }
