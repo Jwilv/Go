6c0ce66843eb8b43aaf3a6fe3c82b263dc812fa2
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-02 19:14:34 -0300

    ejemplo manera de iniciar map

--- ejer10/main.go
+++ ejer10/main.go
@@ -2,18 +2,27 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	//creamops un map con los index de tipo string y va a tener valores tipo string
 	paises := make(map[string]string)
 
-	//setear valores 
+	//setear valores
 	paises["argentina"] = "buenos aires"
 	paises["esp"] = "barcelona"
 
 	fmt.Println(paises)
 
-	//segundo ejemplo 
+	//segundo ejemplo
 
 	//aca le reservamos 5 espacios
 	//paises := make(map[string]string, 5)
+
+	//otra manera de iniciar
+	//sin make
+	//le decimos que los index van a ser string y que va a almacenar tipos de datos int
+	//y le cargamos los valores
+	campeonato := map[string]int{
+		"barcelona": 59,
+		"real madrid": 51,
+	}
 }
