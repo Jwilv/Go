90096af50d41ac7400de5edae5046e338033f8bb
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-05 15:46:10 -0300

    herencia

--- ejer11/main.go
+++ ejer11/main.go
@@ -8,11 +8,12 @@ import (
 )
 
 type juan struct{
-	
+	user.Usuario
 }
 func main(){
 	//establecemos un nuevo objeto usuario
-	
+	user := new(juan)
+	user.AltaUsuario(10, "juan", time.Now(), true)
 }
 
 
