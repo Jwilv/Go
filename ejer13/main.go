60bca1a0fe371a7bb94f1b027599191372f9336e
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-07 15:59:15 -0300

    refactor  de hombre como ambos generos

--- ejer13/main.go
+++ ejer13/main.go
@@ -27,28 +27,22 @@ type hombre struct{
 	respirando bool 
 	pensando bool 
 	comiendo bool 
+	esHombre bool
 }
 
 func (this *hombre) respirar(){ this.respirando = true }
 func (this *hombre) pensar(){ this.pensando = true }
 func (this *hombre) comer(){ this.comiendo = true }
-func (this *hombre) sexo() string{ return "hombre" }
+func (this *hombre) sexo() string{
+	if( this.esHombre == true ){ return "hombre"}
+	return "mujer"
+}
 
 //genero mujer 
 
 type mujer struct{
-	edad int 
-	float float32
-	peso float32
-	respirando bool 
-	pensando bool 
-	comiendo bool 
+	hombre
 }
-
-func (this *mujer) respirar(){ this.respirando = true }
-func (this *mujer) pensar(){ this.pensando = true }
-func (this *mujer) comer(){ this.comiendo = true }
-func (this *mujer) sexo() string{ return "mujer" }
 
 func HumanosRespirando(persona humano){
 persona.respirar()
@@ -57,6 +51,7 @@ fmt.Printf("Soy un/a %s, y estoy respirando \n", persona.sexo())
 
 func main(){
 pedro := new(hombre)
+pedro.esHombre = true 
 HumanosRespirando(pedro)
 maria := new(mujer)
 HumanosRespirando(maria)
