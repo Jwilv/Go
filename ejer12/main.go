b9b4fb7c68b7b0236343164b0c0c4f939ffc0a8e
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-07 15:30:57 -0300

    metodos de hombre

--- ejer12/main.go
+++ ejer12/main.go
@@ -21,12 +21,16 @@ type vegetal interface{
 
 type hombre struct{
 	edad int 
-	float float32
 	peso float32
 	respirando bool 
 	pensando bool 
 	comiendo bool 
 }
+
+func (this *hombre) respirar(){ this.respirando = true }
+func (this *hombre) pensar(){ this.pensando = true }
+func (this *hombre) comer(){ this.comiendo = true }
+func (this *hombre) sexo() string{ return "hombre" }
 
 //genero mujer 
 
