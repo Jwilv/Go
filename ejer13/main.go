a2c2d01d04e5292982269646909e81a04dfed61a
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-07 22:26:20 -0300

    reset

--- ejer13/main.go
+++ ejer13/main.go
@@ -3,81 +3,83 @@ package main
 import "fmt"
 
 //interface
-type humano interface{
+type humano interface {
 	respirar()
 	comer()
 	pensar()
 	sexo() string
 }
-type animal interface{
+type animal interface {
 	respirar()
 	comer()
 	EsCarnivoro() bool
 }
 
-type vegetal interface{
-	ClasificacionVegetal() string 
+type vegetal interface {
+	ClasificacionVegetal() string
 }
 
-//genero hombre 
+//genero hombre
 
-type hombre struct{
-	edad int 
-	peso float32
-	respirando bool 
-	pensando bool 
-	comiendo bool 
-	esHombre bool
+type hombre struct {
+	edad       int
+	peso       float32
+	respirando bool
+	pensando   bool
+	comiendo   bool
+	esHombre   bool
 }
 
-func (this *hombre) respirar(){ this.respirando = true }
-func (this *hombre) pensar(){ this.pensando = true }
-func (this *hombre) comer(){ this.comiendo = true }
-func (this *hombre) sexo() string{
-	if( this.esHombre == true ){ return "hombre"}
+func (this *hombre) respirar() { this.respirando = true }
+func (this *hombre) pensar()   { this.pensando = true }
+func (this *hombre) comer()    { this.comiendo = true }
+func (this *hombre) sexo() string {
+	if this.esHombre == true {
+		return "hombre"
+	}
 	return "mujer"
 }
 
-//genero mujer 
+//genero mujer
 
-type mujer struct{
+type mujer struct {
 	hombre
 }
 
-func HumanosRespirando(persona humano){
-persona.respirar()
-fmt.Printf("Soy un/a %s, y estoy respirando \n", persona.sexo())
+func HumanosRespirando(persona humano) {
+	persona.respirar()
+	fmt.Printf("Soy un/a %s, y estoy respirando \n", persona.sexo())
 }
 
-//mundo animal 
+//mundo animal
 
-type perro struct{
-	comiendo bool 
-	respirando bool 
-	carnivoro bool 
+type perro struct {
+	comiendo   bool
+	respirando bool
+	carnivoro  bool
 }
 
-func (this *perro) comer(){ this.comiendo = true }
-func (this *perro) respirar(){this.respirando = true}
-func (this *perro) EsCarnivoro()bool {return this.carnivoro }
+func (this *perro) comer()            { this.comiendo = true }
+func (this *perro) respirar()         { this.respirando = true }
+func (this *perro) EsCarnivoro() bool { return this.carnivoro }
 
-func animalesRespirando(animal animal){
+func animalesRespirando(animal animal) {
 	animal.respirar()
 	fmt.Println("soy un animal y estoy respirando")
 }
 
 func animalCarnivoro(animal animal) int {
-	if(animal.EsCarnivoro()){
-		return 1 
+	if animal.EsCarnivoro() {
+		return 1
 	}
-	
+
 	return 0
-	}
-	
-func main(){
-pedro := new(hombre)
-pedro.esHombre = true 
-HumanosRespirando(pedro)
-maria := new(mujer)
-HumanosRespirando(maria)
+}
+
+func main() {
+	pedro := new(hombre)
+	pedro.esHombre = true
+	HumanosRespirando(pedro)
+	maria := new(mujer)
+	HumanosRespirando(maria)
 }
