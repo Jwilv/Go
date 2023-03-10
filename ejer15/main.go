983aaeb52834d369b1ca2a443e1dc2f5065d080c
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-10 15:13:18 -0300

    exponencia ej de recursion

--- ejer15/main.go
+++ ejer15/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func main() {
+
+}
+
+func exponencia(numero int) {
+	if numero < 10000000 {
+		return
+	}
+	fmt.Println(numero)
+	exponencia(numero * 2)
 }
