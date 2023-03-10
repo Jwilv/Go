e437067fe0739bb7d4db6be56274ffbf2fb9601f
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-10 15:43:32 -0300

    format

--- ejer16/main.go
+++ ejer16/main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"io/ioutil"
 	"log"
+	"os"
 )
-func main(){
-//defer se ejecuta recien cuando salga de la funcion
-archivo := "archivoFantasma.txt"
-file, err := os.Open(archivo)
-defer file.Close()
-if err != nil{
-	fmt.Println("error al abrir el archivo")
-	os.Exit(1)
-} 
+
+func main() {
+	//defer se ejecuta recien cuando salga de la funcion
+	archivo := "archivoFantasma.txt"
+	file, err := os.Open(archivo)
+	defer file.Close()
+	if err != nil {
+		fmt.Println("error al abrir el archivo")
+		os.Exit(1)
+	}
 }
