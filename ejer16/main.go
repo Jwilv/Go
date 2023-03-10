05f7c3c8ab9c9cbdc2394e3a420146cad6fc756b
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-10 15:42:32 -0300

    ej de defer

--- ejer16/main.go
+++ ejer16/main.go
@@ -7,5 +7,12 @@ import (
 	"log"
 )
 func main(){
-
+//defer se ejecuta cuando salga de la funcion
+archivo := "archivoFantasma.txt"
+file, err := os.Open(archivo)
+defer file.Close()
+if err != nil{
+	fmt.Println("error al abrir el archivo")
+	os.Exit(1)
+} 
 }
