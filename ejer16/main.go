fda9192b1072633d482cea353c4e50bdc117c2f5
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-10 16:05:44 -0300

    reformat

--- ejer16/main.go
+++ ejer16/main.go
@@ -8,31 +8,31 @@ import (
 	"os"
 )
 
-func ejemPanic(){
+func ejemPanic() {
 	//funcio anonima para defer, por que defer solo ejecuta una sola cosa
-	defer func(){
+	defer func() {
 		//recover lee el ultimo panic
 		reco := recover()
-		if reco != nil{
-			log.Fatalf("ocurrio un error que genero un panic \n %v",reco)
+		if reco != nil {
+			log.Fatalf("ocurrio un error que genero un panic \n %v", reco)
 		}
 	}()
-a:=1
-if a==1{
-	//aborta el programa
-	panic("se encontro el valor")
-}
+	a := 1
+	if a == 1 {
+		//aborta el programa
+		panic("se encontro el valor")
+	}
 }
 
-func ejemDefer(){
-		//defer se ejecuta recien cuando salga de la funcion
-		archivo := "archivoFantasma.txt"
-		file, err := os.Open(archivo)
-		defer file.Close()
-		if err != nil {
-			fmt.Println("error al abrir el archivo")
-			os.Exit(1)
-		}
+func ejemDefer() {
+	//defer se ejecuta recien cuando salga de la funcion
+	archivo := "archivoFantasma.txt"
+	file, err := os.Open(archivo)
+	defer file.Close()
+	if err != nil {
+		fmt.Println("error al abrir el archivo")
+		os.Exit(1)
+	}
 }
 
 func main() {
