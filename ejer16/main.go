03637e6966664e1175cbf0fc890f5f96512d8e70
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-10 15:48:14 -0300

    ejemDefer

--- ejer16/main.go
+++ ejer16/main.go
@@ -14,13 +14,17 @@ if a==1{
 }
 }
 
+func ejemDefer(){
+		//defer se ejecuta recien cuando salga de la funcion
+		archivo := "archivoFantasma.txt"
+		file, err := os.Open(archivo)
+		defer file.Close()
+		if err != nil {
+			fmt.Println("error al abrir el archivo")
+			os.Exit(1)
+		}
+}
+
 func main() {
-	//defer se ejecuta recien cuando salga de la funcion
-	archivo := "archivoFantasma.txt"
-	file, err := os.Open(archivo)
-	defer file.Close()
-	if err != nil {
-		fmt.Println("error al abrir el archivo")
-		os.Exit(1)
-	}
+	//ejemDefer()
 }
