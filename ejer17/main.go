6b61051a203f06a7e36eb99678dea27fae67b1f4
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-10 18:51:05 -0300

    ejemplo de funcion lenta

--- ejer17/main.go
+++ ejer17/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func nombreLento(nombre string){
+letras := strings.Split(nombre, "")
+for _, letra := range letras{
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println(letra)
+}
+}
+
 func main(){
-	
+	nombreLento("wilvers")
 }
