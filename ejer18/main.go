e43459cb05d4bef69dae40f88dd992323bfdc2a6
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-12 15:30:52 -0300

    creacion del canal

--- ejer18/main.go
+++ ejer18/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main(){
-	
+import "time"
+
+func main() {
+	//creamos un canal con make
+	//vamos a crear un chan = chanel/canal
+	//de tipo time.Duration
+	canal1 = make(chan time.Duration)
 }
