45ae1de644f1b2d8708794ef373fcfe8462d87d5
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-12 16:30:13 -0300

    server desplegado y listo

--- ejer19/main.go
+++ ejer19/main.go
@@ -9,8 +9,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	//server en el puento 3000
-	http.ListenAndServe(":3000", nil)
 	//ruta principal
 	http.HandleFunc("/", home)
+	//server en el puento 3000
+	//esto es como un bluble por eso las otras instruciones van arriba 
+	http.ListenAndServe(":3000", nil)
 }
