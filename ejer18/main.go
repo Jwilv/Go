b65eb525162c7ccc4418381e2efac206d92358c5
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-12 15:54:27 -0300

    asignar valor a canal

--- ejer18/main.go
+++ ejer18/main.go
@@ -1,10 +1,19 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 //le decimos que el argumento canal va a ser un chan de tipo time.Duration
 func bucle(canal chan time.Duration){
 	inicio := time.Now()
+	for i:=0;i<10000000000;i++{
+
+	}
+	final := time.Now()
+	//le asignamos el valor al canal
+	canal <- final.Sub(inicio)
 }
 
 func main() {
@@ -12,4 +21,5 @@ func main() {
 	//vamos a crear un chan = chanel/canal
 	//de tipo time.Duration
 	canal1 := make(chan time.Duration)
+	fmt.Printf("llegue al final")
 }
