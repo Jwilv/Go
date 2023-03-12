9e4c6fa8ac4212104d87885fa40c8e179f0312a0
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-12 15:47:55 -0300

    parametro

--- ejer18/main.go
+++ ejer18/main.go
@@ -2,6 +2,11 @@ package main
 
 import "time"
 
+//le decimos que el argumento canal va a ser un chan de tipo time.Duration
+func bucle(canal chan time.Duration){
+	inicio := time.Now()
+}
+
 func main() {
 	//creamos un canal con make
 	//vamos a crear un chan = chanel/canal
