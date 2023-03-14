655d61744273c49059c941197f775da01bef4c0e
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-03-13 22:41:20 -0300

    aplicacion de middleware

--- ejer20/main.go
+++ ejer20/main.go
@@ -1,16 +1,20 @@
 package main
 
+import "fmt"
+
 var result int
 
 func main() {
-
+	fmt.Println("inicio")
+	result = middleware(suma)(5,6)
+	fmt.Println(result)
 }
 
 func suma(a, b int) int {
 	return a + b
 }
 
-func resta(a, b int) int {
+func resta(a, b int) int{
 	return a - b
 }
 
@@ -19,7 +23,7 @@ func multiplicacion(a, b int) int {
 }
 
 func middleware(f func(int, int) int) func(int, int) int {
-	return func(a, b int)int{
-		return f(a, b )
+	return func(a, b int) int {
+		return f(a, b)
 	}
 }
